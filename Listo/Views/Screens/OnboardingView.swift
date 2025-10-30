9c7c94172c8a9b3a02fb854ddd693c4daccaa615
styvvve <styvvvenguele@icloud.com>
2025-10-30 19:28:36 +0100

    Onboarding fait totalement, configurer FirebaseAuth et Firebase realtime database

---
+++ Listo/Views/Screens/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Listo
+//
+//  Created by NGUELE Steve  on 30/10/2025.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
